89e0dd1b30f701e78f658f2b77236fac42506d0f
John Doe <johndoe@hp.com>
2020-07-21 22:22:16 +0800

    Blogs Interface Development

--- src/main/java/com/markerhub/controller/BlogController.java
+++ src/main/java/com/markerhub/controller/BlogController.java
@@ -1,9 +1,21 @@
 package com.markerhub.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.markerhub.common.lang.Result;
+import com.markerhub.entity.Blog;
+import com.markerhub.service.BlogService;
+import com.markerhub.util.ShiroUtil;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -14,7 +26,41 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-07-05
  */
 @RestController
-@RequestMapping("/blog")
 public class BlogController {
 
+    @Autowired
+    BlogService blogService;
+    @GetMapping("/blogs")
+    public Result blogs(Integer currentPage) {
+        if(currentPage == null || currentPage < 1) currentPage = 1;
+        Page page = new Page(currentPage, 5);
+        IPage pageData = blogService.page(page, new QueryWrapper<Blog>().orderByDesc("created"));
+        return Result.success(pageData);
+    }
+
+    @GetMapping("/blog/{id}")
+    public Result detail(@PathVariable(name = "id") Long id) {
+        Blog blog = blogService.getById(id);
+        Assert.notNull(blog, "The blog is deleted！");
+        return Result.success(blog);
+    }
+
+    @RequiresAuthentication
+    @PostMapping("/blog/edit")
+    public Result edit(@Validated @RequestBody Blog blog) {
+        System.out.println(blog.toString());
+        Blog temp = null;
+        if(blog.getId() != null) {
+            temp = blogService.getById(blog.getId());
+            Assert.isTrue(temp.getUserId() == ShiroUtil.getProfile().getId(), "No authority to edit!");
+        } else {
+            temp = new Blog();
+            temp.setUserId(ShiroUtil.getProfile().getId());
+            temp.setCreated(LocalDateTime.now());
+            temp.setStatus(0);
+        }
+        BeanUtil.copyProperties(blog, temp, "id", "userId", "created", "status");
+        blogService.saveOrUpdate(temp);
+        return Result.success(null);
+    }
 }
